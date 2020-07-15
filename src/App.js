10291db28d93f90d572cf4bd3f5b2e24619f95cf
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-15 18:31:40 +0530

    add project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='app'>
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
