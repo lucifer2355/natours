8b7289917a0add99514a45062b43e9fecb55c22d
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-30 23:44:06 +0530

    add loren ipsum text

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -4,14 +4,34 @@ function Section() {
   return (
     <main>
       <section className='section-about'>
-        <div className='u-center-text u-margin-bottom-8'>
+        <div className='u-center-text u-margin-bottom-big'>
           <h2 className='heading-secondary'>
             Exciting tours for adventurous people
           </h2>
         </div>
 
         <div className='row'>
-          <div className='col-1-of-2'>Text</div>
+          <div className='col-1-of-2'>
+            <h3 className='heading-tertiry u-margin-botton-small'>
+              You are going to love with nature
+            </h3>
+            <p className='paragraph'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas
+              magni ad, error assumenda provident non numquam consectetur itaque
+              nostrum aperiam ipsam dolore a delectus? Nulla sunt modi
+              perferendis voluptatum facere.
+            </p>
+
+            <h3 className='heading-tertiry u-margin-botton-small'>
+              Live advantures like you never have before
+            </h3>
+            <p className='paragraph'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+              veritatis exercitationem placeat enim ad ipsum, sed dicta at
+              provident. Sint et quos quae officiis, nihil unde molestias
+              debitis maiores officia?
+            </p>
+          </div>
           <div className='col-1-of-2'>Image</div>
         </div>
       </section>
