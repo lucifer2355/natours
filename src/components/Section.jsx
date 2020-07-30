946df6b00e047d964ef1b3e68a4f13ab81890bd3
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-30 23:26:51 +0530

    add column grid for text & images

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,41 +2,55 @@ import React from "react";
 
 function Section() {
   return (
-    <section>
-      <div className='row'>
-        <div className='col-1-of-2'>Col 1 of 2</div>
-        <div className='col-1-of-2'>Col 2 of 2</div>
-      </div>
+    <main>
+      <section className='section-about'>
+        <div className='u-center-text u-margin-bottom-8'>
+          <h2 className='heading-secondary'>
+            Exciting tours for adventurous people
+          </h2>
+        </div>
 
-      <div className='row'>
-        <div className='col-1-of-3'>Col 1 of 3</div>
-        <div className='col-1-of-3'>Col 2 of 3</div>
-        <div className='col-1-of-3'>Col 3 of 3</div>
-      </div>
+        <div className='row'>
+          <div className='col-1-of-2'>Text</div>
+          <div className='col-1-of-2'>Image</div>
+        </div>
+      </section>
+    </main>
+    // <section>
+    //   <div className='row'>
+    //     <div className='col-1-of-2'>Col 1 of 2</div>
+    //     <div className='col-1-of-2'>Col 2 of 2</div>
+    //   </div>
 
-      <div className='row'>
-        <div className='col-1-of-3'>Col 1 of 3</div>
-        <div className='col-2-of-3'>Col 2 of 3</div>
-      </div>
+    //   <div className='row'>
+    //     <div className='col-1-of-3'>Col 1 of 3</div>
+    //     <div className='col-1-of-3'>Col 2 of 3</div>
+    //     <div className='col-1-of-3'>Col 3 of 3</div>
+    //   </div>
 
-      <div className='row'>
-        <div className='col-1-of-4'>Col 1 of 4</div>
-        <div className='col-1-of-4'>Col 2 of 4</div>
-        <div className='col-1-of-4'>Col 3 of 4</div>
-        <div className='col-1-of-4'>Col 4 of 4</div>
-      </div>
+    //   <div className='row'>
+    //     <div className='col-1-of-3'>Col 1 of 3</div>
+    //     <div className='col-2-of-3'>Col 2 of 3</div>
+    //   </div>
 
-      <div className='row'>
-        <div className='col-1-of-4'>Col 1 of 4</div>
-        <div className='col-1-of-4'>Col 2 of 4</div>
-        <div className='col-2-of-4'>Col 3 of 4</div>
-      </div>
+    //   <div className='row'>
+    //     <div className='col-1-of-4'>Col 1 of 4</div>
+    //     <div className='col-1-of-4'>Col 2 of 4</div>
+    //     <div className='col-1-of-4'>Col 3 of 4</div>
+    //     <div className='col-1-of-4'>Col 4 of 4</div>
+    //   </div>
 
-      <div className='row'>
-        <div className='col-1-of-4'>Col 1 of 4</div>
-        <div className='col-3-of-4'>Col 3 of 4</div>
-      </div>
-    </section>
+    //   <div className='row'>
+    //     <div className='col-1-of-4'>Col 1 of 4</div>
+    //     <div className='col-1-of-4'>Col 2 of 4</div>
+    //     <div className='col-2-of-4'>Col 3 of 4</div>
+    //   </div>
+
+    //   <div className='row'>
+    //     <div className='col-1-of-4'>Col 1 of 4</div>
+    //     <div className='col-3-of-4'>Col 3 of 4</div>
+    //   </div>
+    // </section>
   );
 }
 
