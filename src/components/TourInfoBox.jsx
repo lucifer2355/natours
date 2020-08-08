60dcdc6c5e18165bf3253bab8e8c87591c6734a7
lucifer2355 <dgajjar999@gmail.com>
2020-08-09 00:17:43 +0530

    add background image for tours box

--- src/components/TourInfoBox.jsx
+++ src/components/TourInfoBox.jsx
@@ -4,7 +4,11 @@ function TourInfoBox() {
   return (
     <div className='col-1-of-3'>
       <div className='card'>
-        <div className='card__side card__side--front'>FRONT</div>
+        <div className='card__side card__side--front'>
+          <div className='card__picture card__picture--1'>&nbsp;</div>
+          <div className='card__heading'>Heading</div>
+          <div className='card__details'>Details</div>
+        </div>
         <div className='card__side card__side--back card__side--back-1'>
           BACK
         </div>
