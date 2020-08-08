19fbd8a7618b986430e5dddde5daacc1fdf03b50
lucifer2355 <dgajjar999@gmail.com>
2020-08-09 00:40:10 +0530

    add tour heading style

--- src/components/TourInfoBox.jsx
+++ src/components/TourInfoBox.jsx
@@ -6,8 +6,20 @@ function TourInfoBox() {
       <div className='card'>
         <div className='card__side card__side--front'>
           <div className='card__picture card__picture--1'>&nbsp;</div>
-          <div className='card__heading'>Heading</div>
-          <div className='card__details'>Details</div>
+          <h4 className='card__heading'>
+            <span className='card__heading-span card__heading-span--1'>
+              The sea explorer
+            </span>
+          </h4>
+          <div className='card__details'>
+            <ul>
+              <li>3 day tours</li>
+              <li>Up to 30 pepole</li>
+              <li>2 tour guides</li>
+              <li>Sleep in cozy hotels</li>
+              <li>Defficulty: easy</li>
+            </ul>
+          </div>
         </div>
         <div className='card__side card__side--back card__side--back-1'>
           BACK
