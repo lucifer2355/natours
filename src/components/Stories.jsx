883d24f21e80b587c6db745b2b1eeb889c1dceb0
lucifer2355 <dgajjar999@gmail.com>
2020-08-09 23:38:51 +0530

    add story style component

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 function Stories() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <section className='section-stories'>
+      <div className='u-center-text u-margin-bottom-big'>
+        <h2 className='heading-secondary'>We make people genuinely happy</h2>
+      </div>
+
+      <div className='row'>
+        <div className='story'>TEXT</div>
+      </div>
+    </section>
   );
 }
 
