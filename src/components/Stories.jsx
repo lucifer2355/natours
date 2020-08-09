096c36d3eb5be51a214fc2a62e080e845c3c75a7
lucifer2355 <dgajjar999@gmail.com>
2020-08-10 00:08:10 +0530

    add image and shap style

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -8,7 +8,27 @@ function Stories() {
       </div>
 
       <div className='row'>
-        <div className='story'>TEXT</div>
+        <div className='story'>
+          <figure className='story__shape'>
+            <img
+              src='/images/nat-8.jpg'
+              alt='Personon a tour'
+              className='story__img'
+            />
+          </figure>
+          <div className='story__text'>
+            <h3 className='heading-tertiary u-margin-bottom-small'>
+              I had the best week ever with my family
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+              veritatis exercitationem placeat enim ad ipsum, sed dicta at
+              provident. exercitationem placeat enim ad ipsum, sed dicta at
+              provident dolor sit amet consectetur adipisicing elit Minus
+              veritatis exercitationem placeat.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
