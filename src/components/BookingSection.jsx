69e6093648d88a8943d65f53a895436aba144726
lucifer2355 <dgajjar999@gmail.com>
2020-08-14 00:07:20 +0530

    add form component

--- src/components/BookingSection.jsx
+++ src/components/BookingSection.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+import Form from "./Form";
+
 const BookingSection = () => {
   return (
     <section className='section-book'>
       <div className='row'>
-        <div className='book'></div>
+        <div className='book'>
+          <div className='book__form'>
+            <Form />
+          </div>
+        </div>
       </div>
     </section>
   );
---
+++ src/components/Form.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Form = () => {
+  return <form action='#' className='form'></form>;
+};
+
+export default Form;
